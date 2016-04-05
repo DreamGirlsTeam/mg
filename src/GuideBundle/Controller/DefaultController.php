9e3562aa74d9923586132e6a1cd8b151456930d5
Oleksandra Tkalich <oleksandratk@gmail.com>
2016-04-05 14:48:11 +0300

    Fixed view

--- src/GuideBundle/Controller/DefaultController.php
+++ src/GuideBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/y")
+     * @Route("/")
      */
     public function indexAction()
     {
