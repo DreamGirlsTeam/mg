b749df55acfe6f3a715a1e7b424a02295e63a5a3
eleonoria96@gmail.com <eleonoria96@gmail.com>
2016-04-07 02:03:32 +0300

    Initial login form added

---
+++ src/GuideBundle/Entity/Auth.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace GuideBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * Auth
+ *
+ * @ORM\Table(name="auth")
+ * @ORM\Entity(repositoryClass="GuideBundle\Repository\AuthRepository")
+ * @UniqueEntity(fields="username", message="Wrong username")
+ * @UniqueEntity(fields="password", message="Wrong password")
+ */
+class Auth
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="actorId", type="integer", unique=true)
+     */
+    private $actorId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=60, unique=true)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=100, unique=false)
+     */
+    private $password;
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set actorId
+     *
+     * @param integer $actorId
+     *
+     * @return Auth
+     */
+    public function setActorId($actorId)
+    {
+        $this->actorId = $actorId;
+
+        return $this;
+    }
+
+    /**
+     * Get actorId
+     *
+     * @return int
+     */
+    public function getActorId()
+    {
+        return $this->actorId;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return Auth
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+}
+
---
+++ src/GuideBundle/Form/AuthType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace GuideBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class AuthType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('password', TextType::class)
+            ->getForm()
+        ;
+
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'GuideBundle\Entity\Auth'
+        ));
+    }
+}
