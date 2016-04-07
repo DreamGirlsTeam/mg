ba30b248688a631d5aac5d04958c35fa33ce49aa
Oleksandra Tkalich <oleksandratk@gmail.com>
2016-04-07 21:06:13 +0300

    Adding vendor folder

---
+++ vendor/bin/doctrine.bat
@@ -0,0 +1,10 @@
+<<<<<<< HEAD
+@ECHO OFF
+setlocal DISABLEDELAYEDEXPANSION
+SET BIN_TARGET=%~dp0/../doctrine/orm/bin/doctrine
+php "%BIN_TARGET%" %*
+=======
+@ECHO OFF
+SET BIN_TARGET=%~dp0/../doctrine/orm/bin/doctrine
+php "%BIN_TARGET%" %*
+>>>>>>> 45dc6ca738b264e7d01223238dd7755faf0ba382
