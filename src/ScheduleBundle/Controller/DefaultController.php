0d23cbcecc5a4ebe80837c0a139ae21312065cce
eleonoria96@gmail.com <eleonoria96@gmail.com>
2016-05-09 14:04:19 +0300

    Schedule start form styling

--- src/ScheduleBundle/Controller/DefaultController.php
+++ src/ScheduleBundle/Controller/DefaultController.php
@@ -3,15 +3,19 @@
 namespace ScheduleBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use GuideBundle\Form\PatientType;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/reception/schedule", name="schedule")
-     */
-    public function indexAction()
+
+    public function indexAction(Request $request)
     {
-        return $this->render('ScheduleBundle:Default:index.html.twig');
+
+
+        return $this->render('ScheduleBundle:Default:index.html.twig', array(
+               )
+        );
     }
 }
