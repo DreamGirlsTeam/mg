afb931688dbde2b69f4f0d81a47dfa82b7a4f94a
eleonoria96@gmail.com <eleonoria96@gmail.com>
2016-05-14 20:19:59 +0300

    bootstrap Theme added

--- src/ScheduleBundle/Controller/PatientController.php
+++ src/ScheduleBundle/Controller/PatientController.php
@@ -2,15 +2,16 @@
 
 namespace ScheduleBundle\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use ScheduleBundle\Entity\Patient;
+use ScheduleBundle\Entity\Population;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 /**
  * Patient controller.
@@ -27,123 +28,89 @@ class PatientController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $form = $this->createFormBuilder()
-            ->add('number', IntegerType::class, array('label' => 'Кількість пацієнтів', 'attr' => array('class'=>'validate[required,max[20], min[1]]')))
-            ->add('save', SubmitType::class, array('label' => 'Зберегти'))
-            ->getForm();
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            if ($form->getData()) {
-
+        if ($request->isXmlHttpRequest()) {
+            $number = $request->request->get("number");
+            if ($number < 1 || $number > 20) {
+                return new JsonResponse("error",500);
             }
+            $i = 0;
+            $last = false;
+            $form = $this->createFormBuilder()
+                ->add('name', EntityType::class, array(
+                    'label' => 'Тип візиту',
+                    'class' => 'GuideBundle:VisitTypes',
+                    'choices_as_values' => true,
+                    'choice_label' => 'name',
+                ))
+                ->getForm();
+            while ($i != $number) {
+                if ($i == $number - 1) {
+                    $last = true;
+                }
+                $forms[] = $this->renderView('patient/workforms.html.twig', array(
+                    "form" => $form->createView(),
+                    "num" => $i,
+                    "last" => $last
+                ));
+                $i++;
+            }
+
+            return new JsonResponse(array("content" => $forms));
+        } else {
+            $form = $this->createFormBuilder()
+                ->add('number', IntegerType::class, array('label' => 'Кількість пацієнтів', 'attr' => array('class'=>'validate[required,max[20], min[1]]')))
+                ->add('save', SubmitType::class, array('label' => 'Підтвердити'))
+                ->getForm();
+            return $this->render('patient/start.html.twig', array(
+                'form' => $form->createView()
+            ));
         }
-        return $this->render('patient/start.html.twig', array(
-            'form' => $form->createView()
-        ));
     }
 
     /**
-     * Creates a new Patient entity.
+     * Lists all Patient entities.
      *
-     * @Route("/new", name="reception_schedule_new")
+     * @Route("/submit", name="reception_schedule_submit")
+     * @Method("POST")
+     */
+    public function submitAction(Request $request)
+    {
+        if ($request->isXmlHttpRequest()) {
+            $works = $request->request->get('works');
+            $patients = array();
+            $em = $this->getDoctrine()->getEntityManager();
+            //$em->getRepository('ScheduleBundle:Patient');
+            $queryBuilder = $em
+                ->createQueryBuilder()
+                ->delete('ScheduleBundle:Patient', 'a');
+            $queryBuilder->getQuery()->execute();
+            foreach ($works as $id => $work) {
+                $patient = new Patient($id, $work);
+                $em->persist($patient);
+                $em->flush();
+                //$this->patients[] = $patient;
+            }
+            return new JsonResponse(array("process" => true));
+        } else {
+            return new JsonResponse("error",500);
+        }
+    }
+
+    /**
+     * Lists all Patient entities.
+     *
+     * @Route("/result", name="reception_schedule_result")
      * @Method({"GET", "POST"})
      */
-    public function newAction(Request $request)
+    public function resultAction()
     {
-        $patient = new Patient();
-        $form = $this->createForm('ScheduleBundle\Form\PatientType', $patient);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($patient);
-            $em->flush();
-
-            return $this->redirectToRoute('reception_schedule_show', array('id' => $patient->getId()));
-        }
-
-        return $this->render('patient/new.html.twig', array(
-            'patient' => $patient,
-            'form' => $form->createView(),
-        ));
+        $population = $this->buildPopulation()
+        return $this->render('patient/result.html.twig', array());
     }
 
-    /**
-     * Finds and displays a Patient entity.
-     *
-     * @Route("/{id}", name="reception_schedule_show")
-     * @Method("GET")
-     */
-    public function showAction(Patient $patient)
+    private function buildPopulation()
     {
-        $deleteForm = $this->createDeleteForm($patient);
-
-        return $this->render('patient/show.html.twig', array(
-            'patient' => $patient,
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Displays a form to edit an existing Patient entity.
-     *
-     * @Route("/{id}/edit", name="reception_schedule_edit")
-     * @Method({"GET", "POST"})
-     */
-    public function editAction(Request $request, Patient $patient)
-    {
-        $deleteForm = $this->createDeleteForm($patient);
-        $editForm = $this->createForm('ScheduleBundle\Form\PatientType', $patient);
-        $editForm->handleRequest($request);
-
-        if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($patient);
-            $em->flush();
-
-            return $this->redirectToRoute('reception_schedule_edit', array('id' => $patient->getId()));
-        }
-
-        return $this->render('patient/edit.html.twig', array(
-            'patient' => $patient,
-            'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Deletes a Patient entity.
-     *
-     * @Route("/{id}", name="reception_schedule_delete")
-     * @Method("DELETE")
-     */
-    public function deleteAction(Request $request, Patient $patient)
-    {
-        $form = $this->createDeleteForm($patient);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($patient);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('reception_schedule_index');
-    }
-
-    /**
-     * Creates a form to delete a Patient entity.
-     *
-     * @param Patient $patient The Patient entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Patient $patient)
-    {
-        return $this->createFormBuilder()
-            ->setAction($this->generateUrl('reception_schedule_delete', array('id' => $patient->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
+        $em = $this->getDoctrine()->getEntityManager();
+        $individs = $em->getRepository('ScheduleBundle:Patient')->findAll();
     }
 }
---
+++ src/ScheduleBundle/Entity/Population.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Eleonora
+ * Date: 14.05.2016
+ * Time: 20:19
+ */
+
+namespace ScheduleBundle\Entity;
+
+
+class Population
+{
+
+}
